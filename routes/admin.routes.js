3dda97538d8febdcabdb6f3809e2df849584263d
Abdur Rahaman <hptabdurrahaman@gmail.com>
2021-12-08 18:35:01 +0530

    ignore change

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const adminControllers = require("../controllers/admin.controllers");
 const router = express.Router();
 
-router.get("/me", adminControllers.getMe);
+router.get("/login", adminControllers.getMe);
 
 module.exports = router;
