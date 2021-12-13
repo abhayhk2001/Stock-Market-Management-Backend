e22d610a529141e2f52724ad741d197f4be87d7a
Abdur Rahaman <hptabdurrahaman@gmail.com>
2021-12-13 21:02:03 +0530

    initializes and connects to the database

---
+++ dbInteraction/config.js
@@ -0,0 +1,32 @@
+var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'abdurrahaman',
+  database : 'dbmsproj'
+});
+
+module.exports = {
+	executeQuery: function(sql, sqlParam, callback){
+		if(sqlParam == null)
+		{
+			connection.query(sql, function(error, result){
+				if(error)
+				{
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+		else
+		{
+			connection.query(sql, sqlParam, function(error, result){
+				if(error)
+				{
+					console.log(error);
+				}
+				callback(result);
+			});
+		}
+	}
+};
